c37d51aa693e466f3192a9ecad6c5da82aa0f72f
Alisa <sheridaniam@users.noreply.github.com>
2021-05-20 19:16:52 +0300

    Ctrl + Alt + L

--- src/TaskOne.java
+++ src/TaskOne.java
@@ -1,16 +1,13 @@
 public class TaskOne {
     public static void main(String[] args) {
         for (int i = 1; i < 100; i++) {
-            if ((i%3) == 0 && (i%5) == 0) {
+            if ((i % 3) == 0 && (i % 5) == 0) {
                 System.out.print("HelloWorld ");
-            }
-            else if (i%3 == 0) {
+            } else if (i % 3 == 0) {
                 System.out.print("Hello ");
-            }
-            else if (i%5 == 0) {
+            } else if (i % 5 == 0) {
                 System.out.print("World ");
-            }
-            else
+            } else
                 System.out.print(i + " ");
         }
     }
--- src/TaskTwo.java
+++ src/TaskTwo.java
@@ -1,10 +1,10 @@
 public class TaskTwo {
     public static void main(String[] args) {
-        int array [] = new int[10];
+        int array[] = new int[10];
         double summ = 0;
         for (int i = 0; i < array.length; i++) {
-            array [i] = 10 + (int)(Math.random() * 11);
-            summ = summ + array [i];
+            array[i] = 10 + (int) (Math.random() * 11);
+            summ = summ + array[i];
         }
         System.out.println(summ / 10);
     }
